df81367e3314fc2f7eb3d0580f9f6de34d5bf74e
Shreyash7478 <shturkar90@gmail.com>
2019-01-18 23:07:32 +0530

    Solved HP18.cpp

---
+++ HP17.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+int main(){
+    long long t, a,b,n, common , bob ,alice, temp;
+    std::cin>>t;
+    while(t--){
+        common = bob = alice = 0;
+        std::cin>>n>>a>>b;
+        for(int i = 0 ;i < n ;i++){
+            std::cin>>temp;
+            if(temp%a==0)
+            {
+                bob++;
+            }
+            if(temp%b==0)
+            {
+                alice++;
+            }
+            if (temp % a == 0 && temp % b == 0)
+            {
+                common++;
+            }
+        }
+        if(common){
+            bob++;
+        }
+        if(bob> alice){
+            std::cout<<"BOB\n";
+        }else{
+            std::cout<<"ALICE\n";
+        }
+
+    }
+
+}
